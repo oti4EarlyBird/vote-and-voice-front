881a595c974ec35844407113306b5179b43cddac
1suckk <cwsgcd@gmail.com>
2025-08-12 14:46:30 +0900

    0812 1445 controller, webapp 디렉토리 생성

--- src/main/java/controller/MainServlet.java
+++ src/main/java/controller/MainServlet.java
@@ -5,13 +5,12 @@ import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class MainServlet
- */
+//공통으로 처음 진입하면 나오는 메인 화면 페이지입니다.
 @WebServlet("/main")
 public class MainServlet extends HttpServlet {
 	
